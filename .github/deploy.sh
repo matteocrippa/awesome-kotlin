793e423f6020fd405235dd82d980afa9448dac00
Matteo Crippa <matteo@boostco.de>
2017-12-27 15:08:15 +0100

    fix kotlin deploy

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -9,8 +9,8 @@ fi
 
 git checkout master
 
-git config user.name "READMEbot"
-git config user.email "readmebot@users.noreply.github.com"
+git config user.name "Awesome Kotlin"
+git config user.email "awesomekotlin@users.noreply.github.com"
 
 echo add readme
 git add README.md database.json
@@ -19,4 +19,4 @@ echo commit
 git commit -m "[auto] [ci skip] Generate README & Database.json"
 
 echo push
-git push --quiet "https://${GH_TOKEN_DK}@github.com/matteocrippa/awesome-swift" master:master > /dev/null 2>&1
+git push --quiet "https://${GH_TOKEN_DK}@github.com/matteocrippa/awesome-kotlin" master:master > /dev/null 2>&1
